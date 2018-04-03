15422b49f8bc09b088a53e76afaa6484d09d1a8f
Patrick Steffes <psteffes@jump-box.aws.vdc.it.umich.edu>
2018-04-03 09:25:10 -0500

    Add usage

--- aws-saml-api.sh
+++ aws-saml-api.sh
@@ -1,15 +1,22 @@
 duration=14400    # set default session duration to 4 hours
-while getopts ":d:" opt; do
+usage="\nUsage:\n  $0 [-d <session_duration_seconds>]\n"
+while getopts ":d:h" opt; do
   case $opt in
     d)
       duration=$OPTARG
       ;;
+    h)
+      echo -e "$usage"
+      exit 1
+      ;;
     \?)
       echo "Invalid option: -$OPTARG"
+      echo -e "$usage"
       exit 1
       ;;
     :)
       echo "Option -$OPTARG requires an argument."
+      echo -e "$usage"
       exit 1
       ;;
   esac
